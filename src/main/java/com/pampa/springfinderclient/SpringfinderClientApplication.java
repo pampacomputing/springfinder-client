7bf4197613dee49cac347874bb776c8cdb4dd21c
TommDaniel <daninho.tomm@gmail.com>
2025-05-02 19:27:21 -0300

    Initial commit

---
+++ src/main/java/com/pampa/springfinderclient/SpringfinderClientApplication.java
@@ -0,0 +1,13 @@
+package com.pampa.springfinderclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringfinderClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringfinderClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pampa/springfinderclient/SpringfinderClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pampa.springfinderclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringfinderClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
